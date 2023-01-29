fd0a3454b468c0685519da8b7c3aad02a771bd29
Yanir Harel <yanir44321@gmail.com>
2023-01-29 22:36:07 +0200

    feat(): new project

---
+++ guessing_game/target/debug/deps/guessing_game-f2b6b4c306ef6631.d
@@ -0,0 +1,5 @@
+/home/yanir/Documents/rust_stuff/guessing_game/target/debug/deps/guessing_game-f2b6b4c306ef6631.rmeta: src/main.rs
+
+/home/yanir/Documents/rust_stuff/guessing_game/target/debug/deps/guessing_game-f2b6b4c306ef6631.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/yanir/Documents/rust_stuff/guessing_game/target/debug/guessing_game: /home/yanir/Documents/rust_stuff/guessing_game/src/main.rs
